2b1718223221f716e790f88ab5909afdc5bf6536
Yordan Tarazona <yordantarazona9@gmail.com>
2022-09-17 13:09:04 -0500

    Se sube el proyecto ya con frontend en primera face

---
+++ pypSln.App/pypSln.App.consola/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using pypSln.App.dominio;
+using pypSln.App.persistencia;
+namespace pypSln.console
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+        Console.WriteLine("Hello, World!");
+        }
+    }
+}
